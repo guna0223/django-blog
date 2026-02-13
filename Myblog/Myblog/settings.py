681dfd4c808250a515c70ea0e03c5c3f5b5f1d10
guna0223 <gunasekar0223@gmail.com>
2026-02-13 12:37:08 +0530

    update files

--- Myblog/Myblog/settings.py
+++ Myblog/Myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ub28x!uj#)3&g7@e4chxwk70cwpi&x&y+gdsl2_u1an&l-w0hs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gunasekar.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['gunasekar.pythonanywhere.com', '127.0.0.1', 'localhost' ]
 
 
 # Application definition
